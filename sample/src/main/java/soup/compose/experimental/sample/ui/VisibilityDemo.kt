27979e63e8e3d4ce13f8d804de9bc90d2062fe36
SOUP <soupyong@gmail.com>
2021-09-26 19:30:05 +0900

    Introduce Visibility composable

---
+++ sample/src/main/java/soup/compose/experimental/sample/ui/VisibilityDemo.kt
@@ -0,0 +1,129 @@
+/*
+ * Copyright 2021 SOUP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package soup.compose.experimental.sample.ui
+
+import android.content.res.Configuration
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.material.RadioButton
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.luminance
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import soup.compose.experimental.sample.theme.SampleTheme
+import soup.compose.ui.Visibility
+import soup.compose.ui.VisibilityState
+
+@OptIn(ExperimentalComposeUiApi::class)
+@Composable
+fun VisibilityDemo() {
+    val options = VisibilityState.values().toList()
+    val (selectedOption, onOptionSelected) = remember { mutableStateOf(options.first()) }
+    Scaffold(
+        topBar = { TopAppBar(title = { Text(text = "VisibilityDemo") }) },
+        content = {
+            Column(modifier = Modifier.padding(it)) {
+                ColorText("Top", Color.Red)
+                Visibility(visibility = selectedOption) {
+                    ColorText("Visibility", Color.Green)
+                }
+                ColorText("Bottom", Color.Blue)
+
+                Spacer(modifier = Modifier.height(24.dp))
+
+                Row {
+                    ColorText("Left", Color.Red)
+                    Visibility(visibility = selectedOption) {
+                        ColorText("Visibility", Color.Green)
+                    }
+                    ColorText("Right", Color.Blue)
+                }
+            }
+        },
+        bottomBar = { BottomBar(options, selectedOption, onOptionSelected) }
+    )
+}
+
+@Composable
+private fun ColorText(text: String, color: Color) {
+    Text(
+        text = text,
+        color = if (color.luminance() < 0.5) Color.White else Color.Black,
+        modifier = Modifier
+            .background(color)
+            .padding(24.dp)
+    )
+}
+
+@OptIn(ExperimentalComposeUiApi::class)
+@Composable
+private fun BottomBar(
+    options: List<VisibilityState>,
+    selectedOption: VisibilityState,
+    onOptionSelected: (VisibilityState) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(modifier = modifier) {
+        options.forEach { option ->
+            Row(
+                Modifier
+                    .fillMaxWidth()
+                    .height(56.dp)
+                    .selectable(
+                        selected = (option == selectedOption),
+                        onClick = { onOptionSelected(option) },
+                        role = Role.RadioButton
+                    )
+                    .padding(horizontal = 16.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                RadioButton(
+                    selected = (option == selectedOption),
+                    onClick = null
+                )
+                Text(
+                    text = option.name,
+                    modifier = Modifier.padding(start = 16.dp)
+                )
+            }
+        }
+    }
+}
+
+@Preview(name = "Light", showBackground = true)
+@Preview(name = "Dark", showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+private fun Preview() {
+    SampleTheme {
+        VisibilityDemo()
+    }
+}
---
+++ ui/src/main/java/soup/compose/ui/Visibility.kt
@@ -0,0 +1,97 @@
+/*
+ * Copyright 2021 SOUP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package soup.compose.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawWithContent
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.util.fastForEach
+import androidx.compose.ui.util.fastMaxBy
+
+/**
+ * [VisibilityState] contains the same three states as visibility of [android.view.View].
+ *
+ * @sample soup.compose.experimental.sample.ui.VisibilityDemo
+ * @see Visibility
+ */
+@ExperimentalComposeUiApi
+enum class VisibilityState {
+    /**
+     * Same as [android.view.View.VISIBLE].
+     */
+    Visible,
+
+    /**
+     * Same as [android.view.View.INVISIBLE].
+     */
+    Invisible,
+
+    /**
+     * Same as [android.view.View.GONE].
+     */
+    Gone
+}
+
+/**
+ * [Visibility] composable provides the same functionality as the visibility state of a [android.view.View].
+ *
+ * @sample soup.compose.experimental.sample.ui.VisibilityDemo
+ *
+ * @param visibility One of [VisibilityState.Visible], [VisibilityState.Invisible], [VisibilityState.Gone]
+ * @param modifier modifier for the [Layout] created to contain the [content]
+ * @param content Content to appear or disappear based on the value of [visibility]
+ *
+ * @see Visibility
+ * @see VisibilityState
+ */
+@ExperimentalComposeUiApi
+@Composable
+fun Visibility(
+    visibility: VisibilityState,
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit
+) {
+    if (visibility == VisibilityState.Visible || visibility == VisibilityState.Invisible) {
+        Layout(
+            content = { content() },
+            modifier = Modifier
+                .invisible(visibility == VisibilityState.Invisible)
+                .then(modifier)
+        ) { measurables, constraints ->
+            val placeables = measurables.map { it.measure(constraints) }
+            val maxWidth: Int = placeables.fastMaxBy { it.width }?.width ?: 0
+            val maxHeight = placeables.fastMaxBy { it.height }?.height ?: 0
+            layout(maxWidth, maxHeight) {
+                placeables.fastForEach {
+                    it.place(0, 0)
+                }
+            }
+        }
+    }
+}
+
+private fun Modifier.invisible(
+    invisible: Boolean,
+): Modifier {
+    return if (invisible) {
+        // If invisible, only measures but not draws content.
+        drawWithContent { /* drawContent() */ }
+    } else {
+        this
+    }
+}
