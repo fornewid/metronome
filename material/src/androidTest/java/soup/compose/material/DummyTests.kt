2b9f4153f52a832c8f1eeaa5106214ca84a794e6
Sungyong An (SOUP) <soupyong@gmail.com>
2021-09-29 00:25:52 +0900

    Fix 'No tests found' (#4)

---
+++ material/src/androidTest/java/soup/compose/material/DummyTests.kt
@@ -0,0 +1,41 @@
+package soup.compose.material
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+/**
+ * Dummy tests to help with sharding: https://github.com/android/android-test/issues/973
+ */
+@RunWith(JUnit4::class)
+class DummyTests {
+    @Test
+    fun dummy1() = Unit
+
+    @Test
+    fun dummy2() = Unit
+
+    @Test
+    fun dummy3() = Unit
+
+    @Test
+    fun dummy4() = Unit
+
+    @Test
+    fun dummy5() = Unit
+
+    @Test
+    fun dummy6() = Unit
+
+    @Test
+    fun dummy7() = Unit
+
+    @Test
+    fun dummy8() = Unit
+
+    @Test
+    fun dummy9() = Unit
+
+    @Test
+    fun dummy10() = Unit
+}
