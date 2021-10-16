825eafb098d388814924bc2a2c63020d39b8c452
SOUP <soupyong@gmail.com>
2021-10-16 17:29:25 +0900

    [UI] Added Modifier.visible, Modifier.invisible to control visibility of composables

--- ui/src/androidTest/java/soup/compose/ui/VisibilityTest.kt
+++ ui/src/androidTest/java/soup/compose/ui/VisibilityTest.kt
@@ -42,7 +42,7 @@ import org.junit.runner.RunWith
 import soup.compose.experimental.internal.test.assertPixelOfCenter
 
 @RunWith(AndroidJUnit4::class)
-class InvisibleTest {
+class VisibilityTest {
 
     @get:Rule
     val composeTestRule = createComposeRule()
@@ -63,7 +63,69 @@ class InvisibleTest {
     @OptIn(ExperimentalComposeUiApi::class)
     @Test
     @SdkSuppress(minSdkVersion = Build.VERSION_CODES.O) // captureToImage is SDK 26+
-    fun invisible_switchInvisibleState() {
+    fun visible_switchState() {
+        var visible by mutableStateOf(true)
+
+        composeTestRule.setContent {
+            // Red color is displayed only when Green Box is Invisible.
+            Column(
+                modifier = Modifier
+                    .size(20.dp)
+                    .background(color = Color.Red)
+                    .testTag(outerTag)
+            ) {
+
+                // Green Box is displayed on Visible only.
+                Box(
+                    modifier = Modifier
+                        .visible(visible)
+                        .size(20.dp)
+                        .background(color = Color.Green)
+                        .testTag(innerTag)
+                )
+
+                // Blue Box is displayed only when Green Box is Gone.
+                Box(
+                    modifier = Modifier
+                        .size(20.dp)
+                        .background(color = Color.Blue)
+                )
+            }
+        }
+
+        composeTestRule.onNodeWithTag(outerTag)
+            .captureToImage()
+            .assertPixelOfCenter(Color.Green)
+
+        composeTestRule.onNodeWithTag(innerTag)
+            .captureToImage()
+            .assertPixelOfCenter(Color.Green)
+
+        visible = false
+
+        composeTestRule.onNodeWithTag(outerTag)
+            .captureToImage()
+            .assertPixelOfCenter(Color.Red)
+
+        composeTestRule.onNodeWithTag(innerTag)
+            .captureToImage()
+            .assertPixelOfCenter(Color.Red)
+
+        visible = true
+
+        composeTestRule.onNodeWithTag(outerTag)
+            .captureToImage()
+            .assertPixelOfCenter(Color.Green)
+
+        composeTestRule.onNodeWithTag(innerTag)
+            .captureToImage()
+            .assertPixelOfCenter(Color.Green)
+    }
+
+    @OptIn(ExperimentalComposeUiApi::class)
+    @Test
+    @SdkSuppress(minSdkVersion = Build.VERSION_CODES.O) // captureToImage is SDK 26+
+    fun invisible_switchState() {
         var invisible by mutableStateOf(false)
 
         composeTestRule.setContent {
@@ -76,14 +138,13 @@ class InvisibleTest {
             ) {
 
                 // Green Box is displayed on Visible only.
-                Invisible(invisible = invisible) {
-                    Box(
-                        modifier = Modifier
-                            .size(20.dp)
-                            .background(color = Color.Green)
-                            .testTag(innerTag)
-                    )
-                }
+                Box(
+                    modifier = Modifier
+                        .invisible(invisible)
+                        .size(20.dp)
+                        .background(color = Color.Green)
+                        .testTag(innerTag)
+                )
 
                 // Blue Box is displayed only when Green Box is Gone.
                 Box(
