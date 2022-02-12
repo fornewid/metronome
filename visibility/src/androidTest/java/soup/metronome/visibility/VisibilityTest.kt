b2945a968048ff8a1d743c6268cb01387228af11
SOUP <soupyong@gmail.com>
2022-02-12 23:13:54 +0900

    Rename 'compose.extensions' to 'metronome'

---
+++ visibility/src/androidTest/java/soup/metronome/visibility/VisibilityTest.kt
@@ -0,0 +1,183 @@
+/*
+ * Copyright 2021 SOUP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package soup.metronome.visibility
+
+import android.os.Build
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.isDebugInspectorInfoEnabled
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.test.captureToImage
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.unit.dp
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.SdkSuppress
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import soup.metronome.internal.test.assertPixelOfCenter
+
+@RunWith(AndroidJUnit4::class)
+class VisibilityTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    private val outerTag = "outer"
+    private val innerTag = "inner"
+
+    @Before
+    fun before() {
+        isDebugInspectorInfoEnabled = true
+    }
+
+    @After
+    fun after() {
+        isDebugInspectorInfoEnabled = false
+    }
+
+    @Test
+    @SdkSuppress(minSdkVersion = Build.VERSION_CODES.O) // captureToImage is SDK 26+
+    fun visible_switchState() {
+        var visible by mutableStateOf(true)
+
+        composeTestRule.setContent {
+            // Red color is displayed only when Green Box is Invisible.
+            Column(
+                modifier = Modifier
+                    .size(20.dp)
+                    .background(color = Color.Red)
+                    .testTag(outerTag)
+            ) {
+
+                // Green Box is displayed on Visible only.
+                Box(
+                    modifier = Modifier
+                        .testTag(innerTag)
+                        .visible(visible)
+                        .size(20.dp)
+                        .background(color = Color.Green)
+                )
+
+                // Blue Box is displayed only when Green Box is Gone.
+                Box(
+                    modifier = Modifier
+                        .size(20.dp)
+                        .background(color = Color.Blue)
+                )
+            }
+        }
+
+        composeTestRule.onNodeWithTag(outerTag)
+            .captureToImage()
+            .assertPixelOfCenter(Color.Green)
+
+        composeTestRule.onNodeWithTag(innerTag)
+            .captureToImage()
+            .assertPixelOfCenter(Color.Green)
+
+        visible = false
+
+        composeTestRule.onNodeWithTag(outerTag)
+            .captureToImage()
+            .assertPixelOfCenter(Color.Red)
+
+        composeTestRule.onNodeWithTag(innerTag)
+            .captureToImage()
+            .assertPixelOfCenter(Color.Red)
+
+        visible = true
+
+        composeTestRule.onNodeWithTag(outerTag)
+            .captureToImage()
+            .assertPixelOfCenter(Color.Green)
+
+        composeTestRule.onNodeWithTag(innerTag)
+            .captureToImage()
+            .assertPixelOfCenter(Color.Green)
+    }
+
+    @Test
+    @SdkSuppress(minSdkVersion = Build.VERSION_CODES.O) // captureToImage is SDK 26+
+    fun invisible_switchState() {
+        var invisible by mutableStateOf(false)
+
+        composeTestRule.setContent {
+            // Red color is displayed only when Green Box is Invisible.
+            Column(
+                modifier = Modifier
+                    .size(20.dp)
+                    .background(color = Color.Red)
+                    .testTag(outerTag)
+            ) {
+
+                // Green Box is displayed on Visible only.
+                Box(
+                    modifier = Modifier
+                        .testTag(innerTag)
+                        .invisible(invisible)
+                        .size(20.dp)
+                        .background(color = Color.Green)
+                )
+
+                // Blue Box is displayed only when Green Box is Gone.
+                Box(
+                    modifier = Modifier
+                        .size(20.dp)
+                        .background(color = Color.Blue)
+                )
+            }
+        }
+
+        composeTestRule.onNodeWithTag(outerTag)
+            .captureToImage()
+            .assertPixelOfCenter(Color.Green)
+
+        composeTestRule.onNodeWithTag(innerTag)
+            .captureToImage()
+            .assertPixelOfCenter(Color.Green)
+
+        invisible = true
+
+        composeTestRule.onNodeWithTag(outerTag)
+            .captureToImage()
+            .assertPixelOfCenter(Color.Red)
+
+        composeTestRule.onNodeWithTag(innerTag)
+            .captureToImage()
+            .assertPixelOfCenter(Color.Red)
+
+        invisible = false
+
+        composeTestRule.onNodeWithTag(outerTag)
+            .captureToImage()
+            .assertPixelOfCenter(Color.Green)
+
+        composeTestRule.onNodeWithTag(innerTag)
+            .captureToImage()
+            .assertPixelOfCenter(Color.Green)
+    }
+}
---
+++ visibility/src/main/java/soup/metronome/visibility/Visibility.kt
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2021 SOUP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package soup.metronome.visibility
+
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.DrawModifier
+import androidx.compose.ui.graphics.drawscope.ContentDrawScope
+import androidx.compose.ui.layout.LayoutModifier
+import androidx.compose.ui.layout.Measurable
+import androidx.compose.ui.layout.MeasureResult
+import androidx.compose.ui.layout.MeasureScope
+import androidx.compose.ui.layout.Placeable
+import androidx.compose.ui.semantics.clearAndSetSemantics
+import androidx.compose.ui.unit.Constraints
+
+/**
+ * A [Modifier.Element] that controls the content drawing.
+ *
+ * @sample soup.metronome.sample.ui.VisibleSample
+ *
+ * @param visible true sets the visibility to [android.view.View.VISIBLE], false to [android.view.View.INVISIBLE].
+ */
+fun Modifier.visible(visible: Boolean): Modifier {
+    return if (visible) {
+        this
+    } else {
+        this.then(InvisibleModifier)
+            .clearAndSetSemantics {}
+    }
+}
+
+/**
+ * A [Modifier.Element] that controls the content drawing.
+ *
+ * @sample soup.metronome.sample.ui.InvisibleSample
+ *
+ * @param invisible true sets the visibility to [android.view.View.INVISIBLE], false to [android.view.View.VISIBLE].
+ */
+fun Modifier.invisible(invisible: Boolean): Modifier {
+    return if (invisible) {
+        this.then(InvisibleModifier)
+            .clearAndSetSemantics {}
+    } else {
+        this
+    }
+}
+
+private object InvisibleModifier : LayoutModifier, DrawModifier {
+
+    override fun MeasureScope.measure(
+        measurable: Measurable,
+        constraints: Constraints
+    ): MeasureResult {
+        val placeable: Placeable = measurable.measure(constraints)
+        return layout(placeable.width, placeable.height) {
+            placeable.placeRelative(0, 0)
+        }
+    }
+
+    override fun ContentDrawScope.draw() {
+        // only measures but not draws content.
+    }
+}
